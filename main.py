79d35d91cf94a0aff415d08d1353a8017ecdcd6f
chupper100 <phamminhdat223@gmail.com>
2021-11-03 09:37:40 +0700

    root.title()

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-#Updated version
-from tkinter import*
-from tkinter.ttk import*
+# Updated version
+from tkinter import *
+from tkinter.ttk import *
 import requests
 import json
+from time import sleep
+
 
 def screen():
     r = requests.get("https://coronavirus-19-api.herokuapp.com/all")
     data = r.json()
-    text_cases=f'Cases: {data["cases"]}'
-    text_deaths=f'Deaths: {data["deaths"]}'
-    text_recovered=f'Recovered: {data["recovered"]}'
-    label.config(text=(f'{text_cases}\n{text_deaths}\n{text_recovered}'))
+    text_cases = f'Cases: {data["cases"]}'
+    text_deaths = f'Deaths: {data["deaths"]}'
+    text_recovered = f'Recovered: {data["recovered"]}'
+    label.config(text=(f"{text_cases}\n{text_deaths}\n{text_recovered}"))
     label.after(1000, screen)
 
-root = Tk()
-root.title = "CoronaInf4mation"
 
-label = Label(root,font=("sans",80),background='grey',foreground='black')
-label.pack(anchor='center')
+root = Tk()
+root.title("CoronaInf4mation")
+
+label = Label(root, font=("sans", 80), background="grey", foreground="black")
+label.pack(anchor="center")
 screen()
 root.mainloop()
