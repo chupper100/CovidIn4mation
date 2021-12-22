aeda486fcd85e8030b7b451f500e253b84b6c969
chupper100 <phamminhdat223@gmail.com>
2021-12-22 11:06:22 +0700

    update

--- test.py
+++ test.py
@@ -1,66 +1,36 @@
-# Updated version
-from tkinter import *
-from tkinter.ttk import *
 import requests
 import json
-from time import sleep
+from tkinter import *
 
 root = Tk()
-root.title("CoronaInf4mation")
+root.title("Covid-19")
+root.geometry("300x80")
 
-notebook = Notebook(root)
-notebook.pack(pady=15)
+# Including labels
+lbl_1 = Label(root, text="Total confirmed cases: N/A", anchor="e")
+lbl_2 = Label(root, text="Total dead cases: N/A", anchor="e")
+lbl_3 = Label(root, text="Total recovered cases: N/A", anchor="e")
 
-tab_all = Frame(notebook, width=100, height=100)
-tab_vietnam = Frame(notebook, width=100, height=100)
-
-tab_vietnam.pack(fill="both", expand=1)
-tab_all.pack(fill="both", expand=1)
-
-
-notebook.add(tab_all, text="Global")
-notebook.add(tab_vietnam, text="Vietnam")
+lbl_1.grid(column=1, row=0)
+lbl_2.grid(column=1, row=1)
+lbl_3.grid(column=1, row=2)
 
 
-def screen_vietnam():
-    r = requests.get("https://coronavirus-19-api.herokuapp.com/countries/vietnam")
-    data = r.json()
-    text_cases = f'Cases: {"{:,}".format(int(data["cases"]))}'
-    text_deaths = f'Deaths: {"{:,}".format(int(data["deaths"]))}'
-    text_recovered = f'Recoveries: {"{:,}".format(int(data["recovered"]))}'
-    label_vietnam.config(text=(f"{text_cases}\n{text_deaths}\n{text_recovered}"))
-    label_vietnam.after(1000, screen_vietnam)
-
-
-def screen_all():
+def clicked():
+    # Opening the url and loading the
+    # json data using json Library
     r = requests.get("https://coronavirus-19-api.herokuapp.com/all")
     data = r.json()
     text_cases = f'Cases: {"{:,}".format(int(data["cases"]))}'
     text_deaths = f'Deaths: {"{:,}".format(int(data["deaths"]))}'
     text_recovered = f'Recoveries: {"{:,}".format(int(data["recovered"]))}'
-    label_all.config(text=(f"{text_cases}\n{text_deaths}\n{text_recovered}"))
-    label_all.after(1000, screen_all)
+
+    lbl_1.configure(text="Total confirmed cases: " + text_cases)
+    lbl_2.configure(text="Total death cases: " + text_deaths)
+    lbl_3.configure(text="Total recovered cases: " + text_recovered)
 
 
-label_vietnam = Label(
-    tab_vietnam,
-    font=("sans", 80),
-    background="grey",
-    foreground="black",
-    width=200,
-)
-label_all = Label(
-    tab_all,
-    font=("sans", 80),
-    background="grey",
-    foreground="black",
-    width=200,
-)
-
-label_vietnam.pack(anchor="center")
-label_all.pack(anchor="center")
-
-screen_vietnam()
-screen_all()
+btn = Button(root, text="Refresh", command=clicked)
+btn.grid(column=1, row=3)
 
 root.mainloop()
